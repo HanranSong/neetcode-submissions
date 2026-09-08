802b55ac09eba97212a55e67b739273321e355e0
Hanran Song <93172409+HanranSong@users.noreply.github.com>
2026-09-08 15:53:20 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_list = list(s)
+        s_list.sort()
+        t_list = list(t)
+        t_list.sort()
+        return s_list == t_list
